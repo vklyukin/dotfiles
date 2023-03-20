4e69d6732b591f65c643d18dbd1c4566a93193cc
Valeriy Klyukin <valeriy.klyukin@booking.com>
2023-03-20 14:37:16 +0100

    enable zsh plugins

--- init.sh
+++ init.sh
@@ -30,6 +30,7 @@ sudo pip3 install --upgrade \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i $'s/plugins=(git)/plugins=(\\\n\tgit\\\n\tzsh-syntax-highlighting\\\n\tzsh-autosuggestions)/' ~/.zshrc
 
 # dotfiles
 mkdir $HOME/.ssh
