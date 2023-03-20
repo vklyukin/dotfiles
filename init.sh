4d1a6d495ae87563237d16b21850d5fd4e655846
Valeriy Klyukin <valeriy.klyukin@booking.com>
2023-03-20 15:40:01 +0100

    docker

--- init.sh
+++ init.sh
@@ -11,6 +11,20 @@ sudo apt install -y \
     tmux \
     vim \
     zsh
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo apt autoremove
 sudo pip3 install --upgrade pip
 # Sometimes we have problem with TF if we have old fllatbuffers
